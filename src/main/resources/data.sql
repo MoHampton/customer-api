ff6a5153419c2529d3caf83052ad65f98f85262f
Mo <moniquejhampton@yahoo.com>
2020-10-09 20:54:18 -0400

    Added registration and events API

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,20 @@
 
 
-INSERT INTO CUSTOMERS (id, name, email, password) VALUES (0, 'Michael Scott', 'mscott@dundermifflin.com', 'Bo$$')
+INSERT INTO CUSTOMERS (id, name, email, password) VALUES (0, 'Michael Scott', 'mscott@dundermifflin.com', 'Bo$$');
+INSERT INTO CUSTOMERS (id, name, email, password) VALUES (1, 'Pam Beasley', 'pbeasley@dundermifflin.com', 'P@mP@m');
+INSERT INTO CUSTOMERS (id, name, email, password) VALUES (2, 'Jim Halpert', 'jhalpert@dundermifflin.com', 'ILoveP@m');
+
+
+
+insert into EVENTS (EVENT_CODE, TITLE, DESCRIPTION ) VALUES ('CNF001', 'All-Java Conference', 'Lectures and exhibits covering all Java topics' );
+insert into EVENTS (EVENT_CODE, TITLE, DESCRIPTION ) VALUES ('WKS002', 'Spring Boot Workshop', 'Hands-on Spring Boot Workshop' );
+insert into EVENTS (EVENT_CODE, TITLE, DESCRIPTION ) VALUES ('TRN003', 'Angular Training Course', 'Five day introductory training in Angular' );
+
+insert into REGISTRATIONS (EVENT_ID, CUSTOMER_ID, REGISTRATION_DATE, NOTES ) 
+values ( 1, 1, '2019-01-15 00:00:00.0', 'please email me the event details' );
+
+insert into REGISTRATIONS (EVENT_ID, CUSTOMER_ID, REGISTRATION_DATE, NOTES ) 
+values ( 1, 2, '2019-01-17 00:00:00.0', 'looking for info on local hotels' );
+
+insert into REGISTRATIONS (EVENT_ID, CUSTOMER_ID, REGISTRATION_DATE, NOTES ) 
+values ( 1, 3, '2019-01-13 00:00:00.0', 'na' );
